6fb57bdeb9fc68a64517a1b0d5661034d53df2ec
alejandro cortez <ziker@live.com.mx>
2018-09-19 23:29:05 -0500

    computed, watch, methods

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template lang="pug">
   #app
     img(src="./assets/logo.png")
-    h1 {{ msg }}
-    p {{ 1 + 1 }}
-    p {{ 'hola' +  ' Mundo' }}
-    p {{ person.name.toUpperCase() }}
-    p {{ JSON.stringify(person) }}
-    p {{ true ? 'true' : 'false' }}
+    //- p(v-show="showValue") {{ value }}
+    //- p(v-if="showValue") {{ value }}
+    //- p(v-else-if="false") {{ 'Algo más' }}
+    //- p(v-else) {{ 'lo ultimo' }}
+    //- ul
+    //-   li(v-for="i in items") {{ i }}
+    input(v-model="name")
+    button(@click="format") Format
+    p {{ formattedName }}
+    input(v-model="lastName")
+    p {{ fullName }}
+    a(:href="url") Link
+    .field
+      label.label Fecha de nacimiento
+      .control
+        input.input(
+          v-model="birthday"
+          type="date"
+          )
+    .columns
+    p {{ age }}
 </template>
 
 <script>
@@ -17,7 +32,35 @@ export default {
       msg: 'Hello world',
       person: {
         name: 'Alejandro'
-      }
+      },
+      showValue: false,
+      value: 'Algo',
+      items: [ 1, 2, 3, 4, 5 ],
+      name: '',
+      lastName: '',
+      formattedName: '',
+      url: 'https://www.platzi.com',
+      birthday: ''
+    }
+  },
+  computed: {
+    fullName () {
+      return `${this.name} ${this.lastName}`
+    },
+    age () {
+      return new Date(
+        new Date() - new Date(this.birthday)
+      ).getUTCFullYear() - 1970
+    }
+  },
+  watch: {
+    name (newVal, oldVal) {
+      console.log(newVal, oldVal)
+    }
+  },
+  methods: {
+    format () {
+      this.formattedName = this.name.split(' ').join('-').toUpperCase()
     }
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|vue)$/,
+        test: /\.(vue|js)$/,
         loader: 'eslint-loader',
         enforce: 'pre',
         include: [path.resolve(__dirname, './src')]
